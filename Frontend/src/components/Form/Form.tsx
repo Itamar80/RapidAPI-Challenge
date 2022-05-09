f67742876c3d272ece441edbd858d35d77c0712e
itamar80 <itti37@gmail.com>
2022-05-09 13:03:58 +0300

    adding helper function

--- Frontend/src/components/Form/Form.tsx
+++ Frontend/src/components/Form/Form.tsx
@@ -5,6 +5,7 @@ import '../../styles/animations.scss';
 import SearchIcon from '../../assets/search.svg';
 import RapodAPILogo from '../../assets/rapidAPI.svg';
 import { DISABLED, FORM_MOVE_DOWN, FORM_MOVE_UP, ITEM_HIDDEN, ITEM_VISIBLE } from '../../consts/consts';
+import { isValueValid } from '../../helpers/utils';
 type FormProps = {
   getMoviesBySearchTerm: (searchTerm: string) => void;
   moviesStore: MoviesStoreImp;
@@ -13,7 +14,7 @@ type FormProps = {
 
 const Form: React.FC<FormProps> = ({ getMoviesBySearchTerm, moviesStore, inputValue }) => {
   const [searchTerm, setSearchTerm] = useState<string>(inputValue || '');
-  const isButtonDisabled = searchTerm === '' && !moviesStore.isFetching;
+  const isButtonDisabled = !isValueValid(searchTerm) && !moviesStore.isFetching;
   const disabledClass: string = isButtonDisabled ? DISABLED : '';
   const isMoviesInStore: boolean = !!moviesStore.movies.length;
   const formAnimtaionClass: string = isMoviesInStore ? FORM_MOVE_UP : FORM_MOVE_DOWN;
